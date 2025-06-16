2a3698a601228baebc689c4a37b32178c20f8edf
Estimation79 <estimation@dg-inc.qc.ca>
2025-06-16 14:44:09 -0400

    Update __init__.py

--- formulaires/demandes_prix/__init__.py
+++ formulaires/demandes_prix/__init__.py
@@ -1,1 +1,18 @@
+# formulaires/demandes_prix/__init__.py
 
+"""
+Module Demandes de Prix.
+Contient le gestionnaire spécialisé et les interfaces pour RFQ multi-fournisseurs.
+"""
+
+from .gestionnaire_dp import GestionnaireDemandesPrix
+from .interface_dp import render_demandes_prix_tab
+
+__all__ = [
+    'GestionnaireDemandesPrix',
+    'render_demandes_prix_tab'
+]
+
+__version__ = "1.0.0"
+__author__ = "DG Inc. ERP Team"
+__description__ = "RFQ multi-fournisseurs avec comparaison d'offres et sélection automatique du gagnant"
